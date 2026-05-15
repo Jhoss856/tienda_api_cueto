ab01e7ba734ab2b69193a9bb097dd0316f611244
Jhossep Cueto <cuetojhossep@gmail.com>
2026-05-15 08:10:03 -0500

    He agregado un archivo TXT de nombre testing. verifiquen

---
+++ app/Models/Orders.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Orders extends Model
+{
+    /** @use HasFactory<\Database\Factories\OrdersFactory> */
+    use HasFactory;
+    public function client(){
+        return $this->belongToc(Client::class);
+    }
+}
---
+++ app/Models/Products.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Products extends Model
+{
+    /** @use HasFactory<\Database\Factories\ProductsFactory> */
+    use HasFactory;
+    public function category(){
+        return $this->belongTo(category::class,'categories_id');
+    }
+}
